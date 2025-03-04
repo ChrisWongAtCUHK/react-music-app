7a5b4255196c6ed89f4dc91ca8a035388fc2651b
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-04 21:33:48 +0800

    Toggle audio

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,16 @@
-import { useRef } from 'react'
+import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectPlayer, toggleAudio } from '../features/slices/playerSlice'
 
 function Player() {
   const dispatch = useDispatch()
   const player = useSelector(selectPlayer)
-  const playing = useRef(false)
+  const [playing, setPlaying] = useState(false)
 
-  if(player.sound.playing) {
-    playing.current = player.sound.playing
+  function togglePlaying() {
+    dispatch(toggleAudio())
+    setPlaying((pre) => !pre)
   }
-
 
   return (
     <div className='fixed bottom-0 left-0 bg-white px-4 py-4 w-full player-container'>
@@ -27,7 +27,7 @@ function Player() {
         </div>
       ) : null}
        {/* Play/Pause Button */}
-       <button type="button" onClick={() => dispatch(toggleAudio())}>
+       <button type="button" onClick={togglePlaying}>
         <i
           className={['fa', 'text-gray-500', 'text-xl', playing ? 'fa-play' : '', playing ? '' : 'fa-pause'].join(' ')}
         ></i>
