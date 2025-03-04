6179acb64926b7281876884021f9c9178f537b46
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-04 18:06:30 +0800

    Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,36 @@ function Home() {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
-        className='container'
       >
-        <main>Home</main>
+        <main>
+          {/* Introduction */}
+          <section className='mb-8 py-20 text-white text-center relative'>
+            <div
+              className='absolute inset-0 w-full h-full bg-contain introduction-bg'
+              style={{
+                backgroundImage: 'url(assets/img/header.png)',
+                zIndex: -999,
+              }}
+            ></div>
+            <div className='container mx-auto'>
+              <div className='text-white main-header-content'>
+                <h1 className='font-bold text-5xl mb-5'>
+                  Listen to Great Music!
+                </h1>
+                <p className='w-full md:w-8/12 mx-auto'>
+                  Listen to music uploaded by users, Upload your favorite music
+                  now and share them with others!
+                </p>
+              </div>
+            </div>
+
+            <img
+              className='relative block mx-auto mt-5 -mb-20 w-auto max-w-full'
+              src={`/assets/img/introduction-music.png`}
+              alt='introduction'
+            />
+          </section>
+        </main>
       </motion.div>
     </AnimatePresence>
   )
